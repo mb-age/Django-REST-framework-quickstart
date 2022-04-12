e3eeea27946da3e26252b3cec19ee66dc272d74f
mb-age <i.m.angel.777@gmail.com>
2022-04-12 18:08:02 +0200

    Edited settings and urls

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -19,17 +19,13 @@ from rest_framework import routers
 from tutorial.quickstart import views
 
 router = routers.DefaultRouter()
-router.register(r'users', views.UserViewSet)
+router.register(r'users', views.UserViewSet)  # эти 'users' это url: 127.0.0.1:8000/users/
 router.register(r'groups', views.GroupViewSet)
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-]
-
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    path('', include(router.urls)),
+    path('admin/', admin.site.urls),
+    path('', include(router.urls)),  # 'users/' и 'groups/'
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
